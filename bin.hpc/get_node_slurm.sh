15abed00d0c4eb2c20331a77672b992a96a222b4
Jimmy Charnley Kromann <jimmy@charnley.dk>
2021-04-05 18:02:57 +0200

    Cleaned unused files

--- bin.hpc/get_node_slurm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Select partition
-parition=$1
-
-# Get a node for the work day
-srun -p $partition --time=08:00:00 --ntasks-per-node 8 --pty bash
-
--- bin.hpc/jupyter_get_forward.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-port=8765
-frontend=$1
-
-ssh -L $port:localhost:$port $frontend
-
-
